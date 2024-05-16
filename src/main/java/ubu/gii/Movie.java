367763171bb958cdea93a44f57d8be206268346a
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-05-16 13:53:44 +0200

    Metodo getcharge movido desde rental a Movie para solucionar envidia de caracteristicas #3

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -35,4 +35,32 @@ public class Movie {
 	public String getTitle() {
 		return _title;
 	}
+
+	protected double getCharge(Rental rental) {
+		return getCharge(rental.getDaysRented());
+	}
+
+	protected double getCharge(Rental rental, int daysRented) {
+		return getCharge(daysRented);
+	}
+
+	protected double getCharge(int daysRented) {
+		double thisAmount = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
 }
